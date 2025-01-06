05ecf01829988f86a6bb865e2b49d8f4c91d263c
BugHunter-X <askzenai@gmail.com>
2025-01-06 13:04:08 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const { Worker } = require('worker_threads');
+
+const server = http.createServer((req, res) => {
+  const worker = new Worker('./long-task.js');
+
+  worker.on('message', (result) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(`Result: ${result}`);
+  });
+
+  worker.on('error', (err) => {
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end(`Error: ${err.message}`);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
+
+// long-task.js
+const { parentPort } = require('worker_threads');
+
+let result = 0;
+for (let i = 0; i < 1000000000; i++) {
+  result += i;
+}
+parentPort.postMessage(result);
